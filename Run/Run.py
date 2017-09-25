e646567690d95383b41d84bf02c8d783415a476e
PI <t-yaohui.leng@pcitc.com>
2017-09-25 17:28:00 +0800

    try:         InvokeBll.ph_bll().Launch_Page()     except socket.error,ex:         print('执行结束！'+ex.message)

--- Run/Run.py
+++ Run/Run.py
@@ -1,7 +1,10 @@
 #-*-coding=utf-8-*-
 from BLL import InvokeBll
-
+import socket
 
 
 if __name__=="__main__":
-    InvokeBll.ph_bll().Launch_Page()
+    try:
+        InvokeBll.ph_bll().Launch_Page()
+    except socket.error,ex:
+        print('执行结束！'+ex.message)
